f8a549953a8a034c82064f988f2f94a99f31f7c4
ShaneMcC <ShaneMcC@338bfc0d-de2a-0410-874b-4b36cd6df932>
2007-11-20 05:02:38 +0000

    Slight socket changes. SocketWrapper now calls myOwner.close() rather than directly calling socketClosed() sendLine checks SocketChannel.isConnected()
    git-svn-id: https://dfbnc.googlecode.com/svn/trunk@62 338bfc0d-de2a-0410-874b-4b36cd6df932

--- src/uk/org/dataforce/dfbnc/SocketWrapper.java
+++ src/uk/org/dataforce/dfbnc/SocketWrapper.java
@@ -68,6 +68,11 @@ public abstract class SocketWrapper {
 		if (outBuffer == null) { Logger.error("Null outBuffer -> "+line); return; }
 		if (mySocketChannel == null) { Logger.error("Null mySocketChannel -> "+line); return; }
 		if (myOwner == null) { Logger.error("Null myOwner -> "+line); return; }
+		if (!mySocketChannel.isConnected()) {
+			Logger.error("Trying to write to Disconnected SocketChannel -> "+line);
+			myOwner.close(false);
+			return;
+		}
 		
 		synchronized (outBuffer) {
 			outBuffer.append(line+"\r\n");
@@ -234,7 +239,7 @@ public abstract class SocketWrapper {
 			try {
 				write(buf);
 			} catch (IOException e) {
-				myOwner.socketClosed(false);
+				myOwner.close(false);
 			}
 		}
 	}
